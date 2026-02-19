c37f351544f16814e533046bbf17271a6a9dd699
Martin Pilát <48349161+pilatmartin@users.noreply.github.com>
2026-02-19 20:40:56 +0100

    feat: use db pass and secret key from env secrets (#37)

--- backend/app/db/migrations/env.py
+++ backend/app/db/migrations/env.py
@@ -21,9 +21,12 @@ from app.db.models.organism import Organism  # noqa
 from app.db.models.user import User  # noqa
 from app.db.models.stage_preference import DefaultStagePreference, UserStagePreference  # noqa
 
+from app.config import app_config
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy_url", app_config.database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
