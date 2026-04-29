534a9ce08bf8cf06ac413c2de9f22086dcff0d33
Martin Pilat <8pilatmartin8@gmail.com>
2026-04-29 22:21:57 +0200

    fix: throw exception if manual url not configured

--- backend/app/api/v1/routes/docs.py
+++ backend/app/api/v1/routes/docs.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi.responses import RedirectResponse
 
 from app.db.repositories.site_setting import SiteSettingRepository
@@ -14,5 +14,8 @@ async def get_docs(
     """Redirect to the documentation page."""
 
     setting = await settings_repository.get("manual_url")
+
+    if setting is None:
+        raise HTTPException(status_code=404, detail="Manual URL is not configured.")
 
     return RedirectResponse(url=setting.value)
